47f82f91456f964b71c547673cf0fbec3c77b43a
2fresh <45050969+rfrescas@users.noreply.github.com>
2019-04-27 20:59:26 -0500

    Updated html, css, and app.js for better filtering

--- static/js/app.js
+++ static/js/app.js
@@ -4,8 +4,24 @@ const tableData = data;
 // grab element to filter the table
 const filterTable = d3.select("#filter-btn");
 
+// grab element to reset the table
+const resetTable = d3.select("#reset-btn")
+
 // create the variable that will reference the table body
 const tbody = d3.select("tbody");
+
+// create a function to load all data
+
+function loadData() {
+    tableData.forEach(data => {
+        const row = tbody.append("tr");
+        for (const key in data) {
+            row.append("td").text(data[key]);
+        }
+    })
+}
+// initially pre-load the data on the page
+loadData()
 
 // Create a function to build out the table
 filterTable.on("click", function () {
@@ -17,39 +33,32 @@ filterTable.on("click", function () {
     tbody.html("");
 
     // Select the input(s) from user
-    const enterDate = d3.select("#datetime").property("value");
+    const enterDate = d3.select("#datetime").property("value").toLowerCase();
     const enterCity = d3.select("#city").property("value").toLowerCase();
     const enterState = d3.select("#state").property("value").toLowerCase();
     const enterCountry = d3.select("#country").property("value").toLowerCase();
     const enterShape = d3.select("#shape").property("value").toLowerCase();
 
-
     // Use the input to filter the selection
     const byDate = tableData.filter(data => {
         return data.datetime === enterDate
     })
-    console.log(byDate)
 
     const byCity = tableData.filter(city => {
         return city.city === enterCity
     })
-    console.log(byCity)
 
     const byState = tableData.filter(state => {
         return state.state === enterState
     })
-    console.log(byState)
 
     const byCountry = tableData.filter(country => {
         return country.country === enterCountry
     })
-    console.log(byCountry)
-
 
     const byShape = tableData.filter(shape => {
         return shape.shape === enterShape
     })
-    console.log(byShape)
 
     // Loop through the filtered data and put on webpage
     byDate.forEach(data => {
@@ -86,4 +95,15 @@ filterTable.on("click", function () {
             row.append("td").text(shape[key]);
         }
     })
+})
+
+// Create a function to reset the table with a button click
+resetTable.on("click", function () {
+
+    // Prevent default form refresh
+    d3.event.preventDefault();
+    // clear the html tbody for any new data
+    tbody.html("");
+    // reload the data to show everything
+    loadData()
 })
